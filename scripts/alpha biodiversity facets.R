c796486ea30ea41363bdcc57be3a1b48549cd74b
Javi <javiergalandiaz@gmail.com>
2023-09-29 15:57:05 +0200

    export maps

--- scripts/alpha biodiversity facets.R
+++ scripts/alpha biodiversity facets.R
@@ -66,7 +66,7 @@ scl_indices$SES_mpd_rg <- NA #
 
 
 # loop
-for (cl in 1:nrow(scl_indices)) {
+for (cl in 1877:nrow(scl_indices)) {
   
   cell_spp <- scl_occurrences %>% subset(cell==scl_indices$cell[cl]) %>%
     dplyr::select(scientific_name) %>% unique() %>% deframe()
@@ -140,12 +140,14 @@ for (cl in 1:nrow(scl_indices)) {
     
     # standardized effect size: REGIONAL POOL
     if (!(is.na(scl_indices$BIOMExREALM[cl]))) {
-      pool1 <- regional_pool_list[[as.character(scl_indices$BIOMExREALM[cl])]]
-      pool1 <- pool1[[as.character(scl_indices$richness[cl])]]
-      percentile <- ecdf(pool1$Frich_); scl_indices$SES_Frich_rg[cl] <- percentile(scl_indices$Frich_[cl])
-      percentile <- ecdf(pool1$Fdisp_); scl_indices$SES_Fdisp_rg[cl] <- percentile(scl_indices$Fdisp_[cl])
-      percentile <- ecdf(pool1$Faith_); scl_indices$SES_Faith_rg[cl] <- percentile(scl_indices$Faith_[cl])
-      percentile <- ecdf(pool1$mpd_); scl_indices$SES_mpd_rg[cl] <- percentile(scl_indices$mpd_[cl])
+      pool2 <- regional_pool_list[[as.character(scl_indices$BIOMExREALM[cl])]]
+      if (!(is.null(pool2))) { # some rare pools are absent
+        pool2 <- pool2[[as.character(scl_indices$richness[cl])]]
+        percentile <- ecdf(pool2$Frich_); scl_indices$SES_Frich_rg[cl] <- percentile(scl_indices$Frich_[cl])
+        percentile <- ecdf(pool2$Fdisp_); scl_indices$SES_Fdisp_rg[cl] <- percentile(scl_indices$Fdisp_[cl])
+        percentile <- ecdf(pool2$Faith_); scl_indices$SES_Faith_rg[cl] <- percentile(scl_indices$Faith_[cl])
+        percentile <- ecdf(pool2$mpd_); scl_indices$SES_mpd_rg[cl] <- percentile(scl_indices$mpd_[cl])
+      }
     }
   }
   
@@ -157,50 +159,107 @@ for (cl in 1:nrow(scl_indices)) {
 }
 
 
-scl_indices[is.na(scl_indices)] <- 0
-write.table(scl_indices, 'results/scl_indices.txt')
+# scl_indices[is.na(scl_indices)] <- 0
+# write.table(scl_indices, 'results/scl_indices.txt')
 
 
 
 # maps ####
 
 
+# templates to fill
 map_richness <- world_grid; names(map_richness) <- 'richness'
-map_func_richness <- world_grid; names(map_func_richness) <- 'F_rich'
-map_func_dispersion <- world_grid; names(map_func_dispersion) <- 'F_disp'
-map_properenn <- world_grid; names(map_properenn) <- 'prop_perennials'
+
+map_ann_per <- world_grid; names(map_ann_per) <- 'ann_per'
 map_cwm_height <- world_grid; names(map_cwm_height) <- 'CWM_height'
 map_cwm_blade <- world_grid; names(map_cwm_blade) <- 'CWM_bladearea'
 map_cwm_nutlet <- world_grid; names(map_cwm_nutlet) <- 'CWM_nutletvolume'
-map_phylo_richness <- world_grid; names(map_phylo_richness) <- 'Faith'
-map_phylo_diversity <- world_grid; names(map_phylo_diversity) <- 'mpd'
+
+map_Frich <- world_grid; names(map_Frich) <- 'Frich' #   OBSERVED
+map_Fdisp <- world_grid; names(map_Fdisp) <- 'Fdisp'
+map_Faith <- world_grid; names(map_Faith) <- 'Faith'
+map_mpd <- world_grid; names(map_mpd) <- 'mpd'
+
+map_Frich_SESgl <- world_grid; names(map_Frich_SESgl) <- 'SES_Frich_gl' #   SES GLOBAL
+map_Fdisp_SESgl <- world_grid; names(map_Fdisp_SESgl) <- 'SES_Fdisp_gl'
+map_Faith_SESgl <- world_grid; names(map_Faith_SESgl) <- 'SES_Faith_gl'
+map_mpd_SESgl <- world_grid; names(map_mpd_SESgl) <- 'SES_mpd_gl'
+
+map_Frich_SESrg <- world_grid; names(map_Frich_SESrg) <- 'SES_Frich_rg' #   SES REGIONAL
+map_Fdisp_SESrg <- world_grid; names(map_Fdisp_SESrg) <- 'SES_Fdisp_rg'
+map_Faith_SESrg <- world_grid; names(map_Faith_SESrg) <- 'SES_Faith_rg'
+map_mpd_SESrg <- world_grid; names(map_mpd_SESrg) <- 'SES_mpd_rg'
 
 
-map_richness[scl_indices$cell[cl]] <- scl_indices$richness[cl]
-map_func_richness[scl_indices$cell[cl]] <- scl_indices$Frich_[cl]
-map_func_dispersion[scl_indices$cell[cl]] <- scl_indices$Fdisp_[cl]
-map_properenn[scl_indices$cell[cl]] <- scl_indices$ann_per[cl]
-map_propannual[scl_indices$cell[cl]] <- scl_indices$annual_prop[cl]
-map_cwm_height[scl_indices$cell[cl]] <- scl_indices$cwm_height[cl]
-map_cwm_blade[scl_indices$cell[cl]] <- scl_indices$cwm_blade[cl]
-map_cwm_nutlet[scl_indices$cell[cl]] <- scl_indices$cwm_nutlet[cl]
-map_phylo_richness[scl_indices$cell[cl]] <- scl_indices$Faith_[cl]
-map_phylo_diversity[scl_indices$cell[cl]] <- scl_indices$mpd_[cl]
 
-alpha_div_rasters <- c(map_richness, map_func_richness, map_func_dispersion, map_properenn, map_propannual, map_cwm_height, map_cwm_blade, map_cwm_nutlet, map_phylo_richness, map_phylo_diversity)
-writeRaster(alpha_div_rasters, 'results/alpha_div_rasters.tiff', overwrite=TRUE)
+# loop to fill values
+for (cl in 1:nrow(scl_indices)) {
+  
+  map_richness[scl_indices$cell[cl]] <- scl_indices$richness[cl]
+  
+  map_ann_per[scl_indices$cell[cl]] <- scl_indices$ann_per[cl]
+  map_cwm_height[scl_indices$cell[cl]] <- scl_indices$cwm_height[cl]
+  map_cwm_blade[scl_indices$cell[cl]] <- scl_indices$cwm_blade[cl]
+  map_cwm_nutlet[scl_indices$cell[cl]] <- scl_indices$cwm_nutlet[cl]
+  
+  map_Frich[scl_indices$cell[cl]] <- scl_indices$Frich_[cl]
+  map_Fdisp[scl_indices$cell[cl]] <- scl_indices$Fdisp_[cl]
+  map_Faith[scl_indices$cell[cl]] <- scl_indices$Faith_[cl]
+  map_mpd[scl_indices$cell[cl]] <- scl_indices$mpd_[cl]
+  
+  map_Frich_SESgl[scl_indices$cell[cl]] <- scl_indices$SES_Frich_gl[cl]
+  map_Fdisp_SESgl[scl_indices$cell[cl]] <- scl_indices$SES_Fdisp_gl[cl]
+  map_Faith_SESgl[scl_indices$cell[cl]] <- scl_indices$SES_Faith_gl[cl]
+  map_mpd_SESgl[scl_indices$cell[cl]] <- scl_indices$SES_mpd_gl[cl]
+  
+  map_Frich_SESrg[scl_indices$cell[cl]] <- scl_indices$SES_Frich_rg[cl]
+  map_Fdisp_SESrg[scl_indices$cell[cl]] <- scl_indices$SES_Fdisp_rg[cl]
+  map_Faith_SESrg[scl_indices$cell[cl]] <- scl_indices$SES_Faith_rg[cl]
+  map_mpd_SESrg[scl_indices$cell[cl]] <- scl_indices$SES_mpd_rg[cl]
+  
+  
+  # progress
+  print(paste('--- ', round(cl/nrow(scl_indices)*100, 2), '% ---', sep=''))
+  
+}
 
 
-# par(mfrow=c(1,1))
-plot(map_richness, main='Taxonomic richness'); lines(world_lines)
-plot(map_func_richness, main='Functional richness'); lines(world_lines)
-plot(map_func_dispersion, main='Functional dispersion'); lines(world_lines)
-plot(map_phylo_richness, main='Faith`s phylogenetic diversity'); lines(world_lines)
-plot(map_phylo_diversity, main='Phylogenetic mean pairwise distance'); lines(world_lines)
-plot(map_cwm_height, main='Community weighted mean Height'); lines(world_lines)
-plot(map_cwm_blade, main='Community weighted mean Blade area'); lines(world_lines)
-plot(map_cwm_nutlet, main='Community weighted mean Nutlet volume'); lines(world_lines)
-plot(map_propannual, main='Proportion of annual species'); lines(world_lines)
-plot(map_properenn, main='Proportion of perennial species'); lines(world_lines)
+# save rasters
+# alpha_div_rasters <- c(map_richness, map_ann_per, map_cwm_height, map_cwm_blade, map_cwm_nutlet, map_Frich, map_Fdisp, map_Faith, map_mpd, map_Frich_SESgl, map_Fdisp_SESgl, map_Faith_SESgl, map_mpd_SESgl, map_Frich_SESrg, map_Fdisp_SESrg, map_Faith_SESrg, map_mpd_SESrg)
+# writeRaster(alpha_div_rasters, 'results/alpha_div_rasters.tiff', overwrite=TRUE)
+
+
+
+# prepare maps for representation
+
+alpha_div_rasters[['richness']] <- log(alpha_div_rasters[['richness']])
+names(alpha_div_rasters)[1] <- 'log(richness)'
+
+# matrix to reclassify SES maps
+rc_mat <- matrix(nrow=length(seq(0, 1, 0.01)), ncol=2)
+colnames(rc_mat) <- c('from','to')
+rc_mat[,'from'] <- seq(0, 1, 0.01)
+rc_mat[1:6,'to'] <- 1
+rc_mat[7:95,'to'] <- 2
+rc_mat[96:101,'to'] <- 3
+
+v_SES <- c("SES_Frich_gl","SES_Fdisp_gl","SES_Faith_gl","SES_mpd_gl","SES_Frich_rg","SES_Fdisp_rg","SES_Faith_rg","SES_mpd_rg")
+
+# plot
+for (s in 1:dim(alpha_div_rasters)[3]) {
+
+    pdf(file = paste("C:/Users/user/Downloads/",names(alpha_div_rasters)[s],".pdf",sep=""),
+      width = 9.30, # The width of the plot in inches
+      height = 5.74) # The height of the plot in inches
+  
+  alpha_div_rasters[[s]] %>%
+    plot(main=names(alpha_div_rasters)[s], col=rev(grDevices::heat.colors(20)[1:15]))
+  lines(world_lines)
+  
+  dev.off()
+  
+}
+
+
 
 
